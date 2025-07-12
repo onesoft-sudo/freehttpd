348fd8d396a803181379b3f397137cce9c40e6f3
Ar Rakin <rakinar2@onesoftnet.eu.org>
2025-07-12 21:58:17 +0600

    fix: send error handler does not free sockfds correctly

--- src/event/send.c
+++ src/event/send.c
@@ -1,18 +1,18 @@
 /*
  * This file is part of OSN freehttpd.
- * 
+ *
  * Copyright (C) 2025  OSN Developers.
  *
  * OSN freehttpd is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * OSN freehttpd is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with OSN freehttpd.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -23,7 +23,7 @@
 #include "core/conn.h"
 #include "http/protocol.h"
 
-bool 
+bool
 event_send (struct fh_server *server, const xevent_t *event)
 {
 	struct fh_conn *conn = itable_get (server->connections, event->data.fd);
@@ -31,7 +31,7 @@ event_send (struct fh_server *server, const xevent_t *event)
 	if (!conn)
 	{
 		fh_pr_err ("Socket %d does not have an associated connection object", event->data.fd);
-		xpoll_del (server->xpoll_fd, event->data.fd, XPOLLIN);
+		xpoll_del (server->xpoll_fd, event->data.fd, XPOLLOUT);
 		close (event->data.fd);
 		return false;
 	}
