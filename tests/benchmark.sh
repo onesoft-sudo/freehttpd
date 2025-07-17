488e934bdfc6e34b45b4ca9b77e35a97974f1ebc
Ar Rakin <rakinar2@onesoftnet.eu.org>
2025-07-18 00:21:27 +0600

    ci: fix benchmarking issues
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -41,8 +41,7 @@ fi
 echo "Benchmark completed successfully"
 
 kill -9 $pid
-ps aux | grep freehttpd | awk "{ print \$2 }" | xargs kill -9
-wait $pid
+ps aux | grep freehttpd | awk '{ print $2 }' | xargs kill -9
 
 echo "Benchmark completed successfully"
 echo "Check freehttpd.log for details"
