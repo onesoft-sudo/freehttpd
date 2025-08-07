1fe69b746190b06f863122798c426793f8a28bad
Ar Rakin <rakinar2@onesoftnet.eu.org>
2025-08-07 20:21:36 +0600

    feat(freehttpd.c): add command-line option parsing support
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- src/freehttpd.c
+++ src/freehttpd.c
@@ -17,6 +17,10 @@
  * along with OSN freehttpd.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#define _GNU_SOURCE
+#define _DEFAULT_SOURCE
+
+#include <stddef.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdint.h>
@@ -24,11 +28,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <stdbool.h>
 
 #define FH_LOG_MODULE_NAME "main"
 
 #include "core/master.h"
 #include "log/log.h"
+#include "utils/print.h"
 
 #ifdef HAVE_CONFIG_H
 	#include "config.h"
@@ -38,15 +45,19 @@
 	#include <systemd/sd-daemon.h>
 #endif /* FHTTPD_ENABLE_SYSTEMD */
 
-int
-main (int argc, char **argv)
-{
-	if (argc > 1)
-	{
-		fprintf (stderr, "%s: invalid argument -- '%s'\n", argv[0], argv[1]);
-		return 1;
-	}
+static const char *invocation_name = NULL;
 
+static struct option const long_options[] = {
+	{ "help", no_argument, 0, 'h' },
+	{ "version", no_argument, 0, 'V' },
+	{ 0, 0, 0, 0 },
+};
+
+static const char *short_options = "hV";
+
+static int
+start_master (void)
+{
 	struct fh_master *master = fh_master_create ();
 
 	if (!fh_master_read_config (master))
@@ -77,8 +88,67 @@ main (int argc, char **argv)
 	}
 
 	fh_master_wait (master);
-
 	fh_pr_warn ("All worker processes were terminated, exiting");
 	fh_master_destroy (master);
+
 	return 0;
+}
+
+static void
+usage (void)
+{
+	println ("The freehttpd server daemon.");
+	println ("");
+	println ("Usage:");
+	println ("  %s [options]", invocation_name);
+	println ("");
+	println ("Options:");
+	println ("  -h, --help         Print this help and exit");
+	println ("  -V, --version      Print version information");
+	println ();
+	println ("Bug reports and general questions should be sent to "
+			 "<" PACKAGE_BUGREPORT ">.");
+}
+
+static void
+show_version (void)
+{
+	println ("OSN freehttpd version " PACKAGE_VERSION);
+	println ("License: AGPLv3.0 or later, this is free software.");
+}
+
+int
+main (int argc, char **argv)
+{
+	invocation_name = argv[0];
+
+	while (true)
+	{
+		int long_index = 0;
+		int opt = getopt_long (argc, argv, short_options, long_options,
+							   &long_index);
+
+		if (opt == -1)
+			break;
+
+		switch (opt)
+		{
+			case 'h':
+				usage ();
+				exit (EXIT_SUCCESS);
+				break;
+
+			case 'V':
+				show_version ();
+				exit (EXIT_SUCCESS);
+				break;
+
+			case '?':
+			default:
+				exit (EXIT_FAILURE);
+				break;
+		}
+	}
+
+	return start_master ();
 }
